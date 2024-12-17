429b2124e6308f626ac04b4b5a4fc9385a447199
Erti <104018004+Erti14@users.noreply.github.com>
2024-12-17 09:24:33 +0100

    Added method to add numbers

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,9 @@ package org.example;
 
 public class Calculator {
 
+  public static int addNumbers(int number1, int number2){
+        int result = number1 + number2;
+        return result;
+    }
+
 }
