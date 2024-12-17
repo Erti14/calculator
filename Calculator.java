58fa20e96090800042f4bae504bea1b378417564
Erti <104018004+Erti14@users.noreply.github.com>
2024-12-17 09:40:20 +0100

    Added method to subtract numbers

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,9 @@ public class Calculator {
         return result;
     }
 
+  public static int subtractNumbers(int number1, int number2){
+        int result = number1 - number2;
+        return result;
+    }
+
 }
